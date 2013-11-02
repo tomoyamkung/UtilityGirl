f20efd9e48c66192f205bac9e3332267d60defc4
dev4each <dev@4each.biz>
2013-11-02 22:52:39 +0900

    ArrayList クラスの拡張クラスを作成したのでコミット。

---
+++ src/main/java/net/tomoyamkung/library/util/ExtArrayList.java
@@ -0,0 +1,29 @@
+package net.tomoyamkung.library.util;
+
+import java.util.ArrayList;
+
+/**
+ * <code>ArrayList</code> を拡張したクラス。
+ * 
+ * <code>ArrayList#add</code> を連続して書けるように拡張したクラス。
+ * 
+ * @author tomoyamkung
+ *
+ * @param <E> 型パラメータ
+ */
+public class ExtArrayList<E> extends ArrayList<E> {
+
+	private static final long serialVersionUID = -3323720716588400248L;
+
+	/**
+	 * 要素を追加する。
+	 * 
+	 * @param element 要素
+	 * @return
+	 */
+	public ExtArrayList<E> addThis(E element) {
+		this.add(element);
+		return this;
+	}
+
+}
