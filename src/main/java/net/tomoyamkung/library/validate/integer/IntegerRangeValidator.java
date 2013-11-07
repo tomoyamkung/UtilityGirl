28b5b7e2327666ab1441298f5fcbf0fa64cecf0f
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2013-11-07 18:20:15 +0900

    IntegerRangeValidator を実装したのでコミット。

---
+++ src/main/java/net/tomoyamkung/library/validate/integer/IntegerRangeValidator.java
@@ -0,0 +1,40 @@
+package net.tomoyamkung.library.validate.integer;
+
+import net.tomoyamkung.library.util.IntegerUtil;
+import net.tomoyamkung.library.validate.Validator;
+
+/**
+ * 値が指定の範囲内であることを確認するクラス。
+ * 
+ * @author tomoyamkung
+ *
+ */
+public class IntegerRangeValidator implements Validator {
+	
+	/**
+	 * 最小値。
+	 */
+	private int min;
+	
+	/**
+	 * 最大値。
+	 */
+	private int max;
+	
+	/**
+	 * 値のとりうる範囲を設定する。
+	 * 
+	 * @param min 最小値
+	 * @param max 最大値
+	 */
+	public IntegerRangeValidator(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	@Override
+	public boolean execute(String value) {
+		return IntegerUtil.isInRange(value, min, max);
+	}
+
+}
---
+++ src/test/java/net/tomoyamkung/library/validate/integer/IntegerRangeValidatorTest.java
@@ -0,0 +1,41 @@
+package net.tomoyamkung.library.validate.integer;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import net.tomoyamkung.library.validate.Fixture;
+import net.tomoyamkung.library.validate.Validator;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class IntegerRangeValidatorTest {
+	
+	private static final Integer MAX_VALUE = 9;
+	private static final Integer MIN_VALUE = 0;
+	
+	@DataPoints
+	public static Fixture[] PARAMS = {
+		new Fixture(null, false),
+		new Fixture("", false),
+		new Fixture(MIN_VALUE.toString(), true), // 最小値と同じ
+		new Fixture(String.valueOf(MIN_VALUE - 1), false), // 最小値より1つ小さい
+		new Fixture(MAX_VALUE.toString(), true), // 最大値と同じ
+		new Fixture(String.valueOf(MAX_VALUE + 1), false), // 最大値より1つ大きい
+	};
+
+	@Theory
+	public void test(Fixture p) throws Exception {
+		// Setup
+		Validator sut = new IntegerRangeValidator(MIN_VALUE, MAX_VALUE);
+		
+		// Exercise
+		boolean actual = sut.execute(p.target);
+
+		// Verify
+		assertThat(actual, is(p.expected));
+	}
+
+}
