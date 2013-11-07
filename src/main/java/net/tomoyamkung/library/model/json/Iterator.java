abcd203b6baac1c4d829bb0878ed3533ae8c629b
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2013-11-07 12:59:29 +0900

    Iterator インタフェースを作成したのでコミット。

---
+++ src/main/java/net/tomoyamkung/library/model/json/Iterator.java
@@ -0,0 +1,26 @@
+package net.tomoyamkung.library.model.json;
+
+/**
+ * Iterator インタフェース。
+ * 
+ * @author tomoyamkung
+ *
+ * @param <T> <code>Iterator</code> が返すオブジェクト
+ */
+public interface Iterator<T> {
+	
+	/**
+	 * 次の要素があるかを問い合わせる。
+	 * 
+	 * @return 次の要素がある場合 true
+	 */
+	public boolean hasNext();
+	
+	/**
+	 * 次の要素を取得する。
+	 * 
+	 * @return
+	 */
+	public T next();
+
+}
