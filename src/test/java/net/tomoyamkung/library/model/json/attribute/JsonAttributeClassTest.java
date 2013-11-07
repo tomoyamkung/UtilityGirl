3d315ea9dd36f1c0debd0bd0aba6111f770fbab9
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2013-11-07 14:43:52 +0900

    リファクタリングしたのでコミット。

--- src/test/java/net/tomoyamkung/library/model/json/attribute/JsonAttributeClassTest.java
+++ src/test/java/net/tomoyamkung/library/model/json/attribute/JsonAttributeClassTest.java
@@ -2,6 +2,7 @@ package net.tomoyamkung.library.model.json.attribute;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import net.tomoyamkung.library.model.json.DummyJson;
 import net.tomoyamkung.library.util.JsonUtil;
 
 import org.junit.Test;
@@ -11,7 +12,7 @@ public class JsonAttributeClassTest {
 	@Test
 	public void test() throws Exception {
 		// Setup
-		String json = JsonUtil.serialize(DummyJson.create(), "");
+		String json = JsonUtil.serialize(DummyJson.newInstance(), "");
 		
 		// Exercise
 		JsonAttributeClass sut = new JsonAttributeClass(json);
@@ -22,40 +23,6 @@ public class JsonAttributeClassTest {
 		assertThat(sut.get("attr3").getValue(), is("value3"));
 		assertThat("定義されていない属性名称を指定した場合は null になる",
 				sut.get("x"), is(nullValue(JsonAttribute.class)));
-	}
-	
-	static class DummyJson {
-		private String attr1 = "value1";
-		private String attr2 = "value2";
-		private String attr3 = "value3";
-		
-		public static DummyJson create() {
-			return new DummyJson();
-		}
-
-		public String getAttr1() {
-			return attr1;
-		}
-
-		public void setAttr1(String attr1) {
-			this.attr1 = attr1;
-		}
-
-		public String getAttr2() {
-			return attr2;
-		}
-
-		public void setAttr2(String attr2) {
-			this.attr2 = attr2;
-		}
-
-		public String getAttr3() {
-			return attr3;
-		}
-
-		public void setAttr3(String attr3) {
-			this.attr3 = attr3;
-		}
 	}
 
 }
