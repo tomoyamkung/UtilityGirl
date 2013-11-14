ab1bd352c6fe2ebe714b32793848f710ade06b0d
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2013-11-14 10:25:56 +0900

    ListUtil#copy を実装したのでコミット。

---
+++ src/main/java/net/tomoyamkung/library/util/ListUtil.java
@@ -0,0 +1,56 @@
+package net.tomoyamkung.library.util;
+
+import java.util.List;
+
+/**
+ * List に関するユーティリティクラス。
+ * 
+ * @author tomoyamkung
+ * 
+ */
+public class ListUtil {
+
+	/**
+	 * インデックスの位置にあるオブジェクトを末尾に追加する。
+	 * 
+	 * 次の場合は何も処理を行わないで終了する。
+	 * 
+	 * <ul>
+	 * <li><code>from</code> が <code>null</code> または空の場合</li>
+	 * <li><code>from</code> のサイズよりも <code>index</code> のほうが大きい場合</li>
+	 * </ul>
+	 * 
+	 * @param from
+	 *            コピーされるリスト
+	 * @param index
+	 *            コピーするオブジェクトのインデックス
+	 * @param to
+	 *            追加されるリスト
+	 */
+	public static <T> void copy(List<T> from, int index, List<T> to) {
+		if (isNullOrEmpty(from)) {
+			return;
+		}
+		if (from.size() <= index) {
+			return;
+		}
+
+		to.add(from.get(index));
+
+	}
+
+	/**
+	 * リストが null または空であるかを問い合わせる。
+	 * 
+	 * @param list
+	 *            検査するリスト
+	 * @return null または空の場合 true
+	 */
+	private static <T> boolean isNullOrEmpty(List<T> list) {
+		if (list == null || list.isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ src/test/java/net/tomoyamkung/library/util/ListUtilTest.java
@@ -0,0 +1,77 @@
+package net.tomoyamkung.library.util;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class ListUtilTest {
+	
+	public static class Copy {
+		
+		@Test
+		public void toの中身が空の場合() throws Exception {
+			
+			// Setup
+			List<String> from = new ExtArrayList<String>().addThis("1").addThis("2").addThis("3");
+			List<String> to = new ArrayList<String>();
+			
+			// Exercise
+			ListUtil.copy(from, 1, to);
+			
+			// Verify
+			assertThat(to.size(), is(1));
+			assertThat(to.get(0), is("2"));
+		}
+		
+		@Test
+		public void toにオブジェクトが格納されている場合() throws Exception {
+			
+			// Setup
+			List<String> from = new ExtArrayList<String>().addThis("1").addThis("2").addThis("3");
+			List<String> to = new ExtArrayList<String>().addThis("a").addThis("b").addThis("c");
+			
+			// Exercise
+			ListUtil.copy(from, 1, to);
+			
+			// Verify
+			assertThat(to.size(), is(4));
+			assertThat(to.get(3), is("2"));
+		}
+		
+		@Test
+		public void fromの中身が空の場合がnullの場合() throws Exception {
+			
+			// Setup
+			List<String> from = new ArrayList<String>();
+			List<String> to = new ArrayList<String>();
+			
+			// Exercise
+			ListUtil.copy(from, 1, to);
+			
+			// Verify
+			assertThat(to.size(), is(0));
+		}
+		
+		@Test
+		public void fromのサイズよりもインデックスのほうが大きい場合() throws Exception {
+			
+			// Setup
+			List<String> from = new ExtArrayList<String>().addThis("1");
+			List<String> to = new ArrayList<String>();
+			
+			// Exercise
+			ListUtil.copy(from, 1, to);
+			
+			// Verify
+			assertThat(to.size(), is(0));
+		}
+	}
+
+}
